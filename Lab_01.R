9fa382d3bf0f8a89279d262328efac11fd442cd7
vladyslavBrothervinn <vladyslavbezb01@gmail.com>
2025-03-06 23:16:03 +0100

    lab 01

--- Lab_01.R
+++ Lab_01.R
@@ -1,3 +1,5 @@
+#Lab 01 Bezborodov
+
 # Installation of required packages
 packages <- c("MASS", "car", "readxl", "rgl", "rmarkdown", "nortest",
               "latex2exp", "pca3d", "ISLR", "pls", "corrplot", "glmnet",
@@ -8,9 +10,9 @@ install.packages(packages)
 
 # Load packages
 lapply(packages, library, character.only = TRUE)
-
-data <- read.table(file = "gym_members_exercise_tracking.csv", header = TRUE, sep = ",")
-
+setwd("C:/Users/Cyberpower/r_studio/ukma/big_data_analytics")
+data <- read.table(file = "data/gym_members_exercise_tracking.csv", header = TRUE, sep = ",")
+data
 # Описовий аналіз для кожної змінної
 # Визначимо тип змінних (якісна/кількісна)
 str(data)
@@ -18,7 +20,7 @@ summary(data)
 
 # Для кількісних змінних
 quant_vars <- sapply(data, is.numeric)
-
+quant_vars
 Mean = sapply(data[, quant_vars], mean, na.rm = TRUE)
 Median = sapply(data[, quant_vars], median, na.rm = TRUE)
 SD = sapply(data[, quant_vars], sd, na.rm = TRUE)
@@ -44,6 +46,9 @@ missing_values
 
 
 # Створення діаграми розсіювання для всіх змінних у наборі даних
+install.packages("car")
+library(car)
+
 scatterplotMatrix(data[sapply(data, is.numeric)], 
                   col = 1,  
                   regLine = list(col = 2), 
@@ -68,4 +73,167 @@ for (i in 1:ncol(data_numeric)) {
   
   cat("R² для змінної", names(data_numeric)[i], ":", summary(model)$r.squared, "\n\n")
 }
+######################################################################################
+#  Task2 Перевірка припущень
+# BMI ~ Age
+bmi_age <- lm(BMI ~ Age, data = data)
+sm<-summary(bmi_age)
+sm
+
+plot(data$Age, data$BMI)
+abline(bmi_age, col=2)
+
+sum(resid(bmi_age)^ 2)
+var(data$Age)
+var(data$BMI)
+hist(data$Age)
+hist(data$BMI)
+plot(sm$residuals)
+abline(h = 0, col = "red")
+mean(sm$residuals)
+var(sm$residuals)
+hist(sm$residuals)
+
+# BMI ~ Weight..kg.
+bmi_weight <- lm(BMI ~ Weight..kg., data = data)
+sm2<-summary(bmi_weight)
+sm2
+
+plot(data$Weight..kg., data$BMI)
+abline(bmi_weight, col=2)
+
+sum(resid(bmi_weight)^ 2)
+var(data$Weight..kg.)
+var(data$BMI)
+hist(data$Weight..kg.)
+hist(data$BMI)
+plot(sm2$residuals)
+abline(h = 0, col = "red")
+mean(sm2$residuals)
+var(sm2$residuals)
+hist(sm2$residuals)
+
+# Weight ~ Height..m.
+weight_height <- lm(Weight..kg. ~ Height..m., data = data)
+sm3<-summary(weight_height)
+sm3
+
+plot(data$Height..m., data$Weight..kg.)
+abline(weight_height, col=2)
+
+sum(resid(weight_height)^ 2)
+var(data$Height..m.)
+var(data$Weight..kg.)
+hist(data$Height..m.)
+hist(data$Weight..kg.)
+plot(sm3$residuals)
+abline(h = 0, col = "red")
+mean(sm3$residuals)
+var(sm3$residuals)
+hist(sm3$residuals)
+
+# Height..m. ~ Water_Intake..liters.
+height_water_intake <- lm(Height..m. ~ Water_Intake..liters., data = data)
+sm4<-summary(height_water_intake)
+sm4
+
+plot(data$Height..m., data$Water_Intake..liters.)
+abline(height_water_intake, col=2)
+
+sum(resid(height_water_intake)^ 2)
+var(data$Water_Intake..liters.)
+var(data$Height..m.)
+hist(data$Water_Intake..liters.)
+hist(data$Height..m.)
+plot(sm4$residuals)
+abline(h = 0, col = "red")
+mean(sm4$residuals)
+var(sm4$residuals)
+hist(sm4$residuals)
+
+# Calories_Burned ~ Session_Duration..hours.
+session_dur_calories <- lm(Session_Duration..hours. ~ Calories_Burned, data = data)
+sm5<-summary(session_dur_calories)
+sm5
+
+plot(data$Calories_Burned, data$Session_Duration..hours.)
+abline(coef = sm5$coefficients, col="red")
+
+sum(resid(session_dur_calories)^ 2)
+var(data$Calories_Burned)
+var(data$Session_Duration..hours.)
+hist(data$Calories_Burned)
+hist(data$Session_Duration..hours.)
+plot(sm5$residuals)
+abline(h = 0, col = "red")
+mean(sm5$residuals)
+var(sm5$residuals)
+hist(sm5$residuals)
+
+# Calories_Burned ~ Fat_Percentage
+calories_fat <- lm(Calories_Burned ~ Fat_Percentage, data = data)
+sm6<-summary(calories_fat)
+sm6
+
+plot(data$Fat_Percentage, data$Calories_Burned)
+abline(coef = sm6$coefficients, col="red")
+
+sum(resid(calories_fat)^ 2)
+var(data$Fat_Percentage)
+var(data$Calories_Burned)
+hist(data$Fat_Percentage)
+hist(data$Calories_Burned)
+plot(sm6$residuals)
+abline(h = 0, col = "red")
+mean(sm6$residuals)
+var(sm6$residuals)
+hist(sm6$residuals)
+# Fat_Percentage ~ Water_Intake..liters.
+fat_water <- lm(Fat_Percentage ~ Water_Intake..liters., data = data)
+sm7<-summary(fat_water)
+sm7
+
+plot(data$Water_Intake..liters., data$Fat_Percentage)
+abline(coef = sm7$coefficients, col="red")
+
+sum(resid(fat_water)^ 2)
+var(data$Water_Intake..liters.)
+var(data$Fat_Percentage)
+hist(data$Water_Intake..liters.)
+hist(data$Fat_Percentage)
+plot(sm7$residuals)
+abline(h = 0, col = "red")
+mean(sm7$residuals)
+var(sm7$residuals)
+hist(sm7$residuals)
+
+# Experience_Level ~ Workout_Frequency..days.week.
+exp_freq <- lm(Experience_Level ~ Workout_Frequency..days.week., data = data)
+sm8<-summary(exp_freq)
+sm8
+
+plot(data$Workout_Frequency..days.week., data$Experience_Level)
+abline(coef = sm8$coefficients, col="red")
+
+sum(resid(exp_freq)^ 2)
+var(data$Workout_Frequency..days.week.)
+var(data$Experience_Level)
+hist(data$Workout_Frequency..days.week.)
+hist(data$Experience_Level)
+plot(sm8$residuals)
+abline(h = 0, col = "red")
+mean(sm8$residuals)
+var(sm8$residuals)
+hist(sm8$residuals)
+
+
+
+
+
+
+
+
+
+
+
 
